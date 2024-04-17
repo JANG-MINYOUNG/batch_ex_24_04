0832c362f7ded8b118bd00e873d20732871a8e48
JANG-MINYOUNG <wkdalsdudqq@gmail.com>
2024-04-17 15:28:12 +0900

    withParamJob 추가, 실행 시 program arguments 활용

--- src/main/java/com/koreait/exam/batch_ex_24_04/job/HelloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/koreait/exam/batch_ex_24_04/job/HelloWorld/HelloWorldJobConfig.java
@@ -1,13 +1,10 @@
-package com.koreait.exam.batch_ex_24_04.job.HelloWorld;
+package com.koreait.exam.batch_ex_24_04.job.withParam;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -15,57 +12,29 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class HelloWorldJobConfig {
+public class WithParamJobConfig {
   private final JobBuilderFactory jobBuilderFactory;
   private final StepBuilderFactory stepBuilderFactory;
 
   @Bean
-  public Job helloWorldJob() {
-    return jobBuilderFactory.get("helloWorldJob")
+  public Job withParamJob() {
+    return jobBuilderFactory.get("withParamJob")
 //                .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID 를 실행할 때 파라미터로 부여
-        .start(helloWorldStep1())
-        .next(helloWorldStep2())
+        .start(withParamStep1())
         .build();
   }
 
-  @Bean
-  @JobScope
-  public Step helloWorldStep1() {
-    return stepBuilderFactory.get("helloWorldStep1")
-        .tasklet(helloWorldStep1Tasklet())
+  private Step withParamStep1() {
+    return stepBuilderFactory.get("withParamStep1")
+        .tasklet(withParamStep1Tasklet())
         .build();
+
   }
 
-  @Bean
-  @StepScope
-  public Tasklet helloWorldStep1Tasklet() {
+  private Tasklet withParamStep1Tasklet() {
     return (stepContribution, chunkContext) -> {
-      System.out.println("헬로월드 테스클릿 1!!!");
+      System.out.println("withParam 테스클릿 1!!!");
       return RepeatStatus.FINISHED;
     };
   }
-
-  @Bean
-  @JobScope
-  public Step helloWorldStep2() {
-    return stepBuilderFactory.get("helloWorldStep2")
-        .tasklet(helloWorldStep2Tasklet())
-        .build();
-  }
-
-  @Bean
-  @StepScope
-  public Tasklet helloWorldStep2Tasklet() {
-    return (stepContribution, chunkContext) -> {
-
-      System.out.println("헬로월드 테스클릿 2!!!");
-
-      if (false) {
-        throw new Exception("실패 : 헬로월드 테스클릿 2");
-      }
-
-      return RepeatStatus.FINISHED;
-    };
-  }
-
 }
